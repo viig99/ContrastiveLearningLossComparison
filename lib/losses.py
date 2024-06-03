417ab6d3593990ee955191ac72eb27cb4cfadce2
viig99 <accio.arjun@gmail.com>
2024-06-03 11:28:47 -0400

    Adding rest losses.

--- lib/losses.py
+++ lib/losses.py
@@ -37,12 +37,12 @@ class DCL(torch.nn.Module):
         # u: shape (B, D) where B is the batch size and D is the feature dimension
         # v: shape (B, D) where B is the batch size and D is the feature dimension
 
-        B, _ = u.shape
+        B = u.size(0)
         sim_uv = sim(u, v, self.temperature)
         sim_uu = sim(u, u, self.temperature)
 
         pos_mask = torch.eye(B, device=u.device, dtype=torch.bool)
-        pos_loss = -sim_uv.masked_select(pos_mask)
+        pos_loss = sim_uv.masked_select(pos_mask)
 
         neg_mask = ~pos_mask
         neg_sim_uv = sim_uv.masked_select(neg_mask).view(B, -1)
@@ -50,7 +50,7 @@ class DCL(torch.nn.Module):
         negative_sim = torch.cat((neg_sim_uv, neg_sim_uu), dim=1)
         neg_loss = torch.logsumexp(negative_sim, dim=-1)
 
-        return (pos_loss + neg_loss).mean()
+        return (-pos_loss + neg_loss).mean()
 
 
 class DHEL(torch.nn.Module):
@@ -62,17 +62,40 @@ class DHEL(torch.nn.Module):
         # u: shape (B, D) where B is the batch size and D is the feature dimension
         # v: shape (B, D) where B is the batch size and D is the feature dimension
 
-        B, _ = u.shape
+        B = u.size(0)
         sim_uv = sim(u, v, self.temperature)
         sim_uu = sim(u, u, self.temperature)
 
         pos_mask = torch.eye(B, device=u.device, dtype=torch.bool)
-        pos_loss = -sim_uv.masked_select(pos_mask)
+        pos_loss = sim_uv.masked_select(pos_mask)
 
-        neg_sim_uu = -sim_uu[~pos_mask].view(B, -1)
+        neg_sim_uu = sim_uu[~pos_mask].view(B, -1)
         neg_loss = torch.logsumexp(neg_sim_uu, dim=-1)
 
-        return (pos_loss + neg_loss).mean()
+        return (-pos_loss + neg_loss).mean()
+
+
+class NT_xent(torch.nn.Module):
+    def __init__(self, temperature: float = 0.07):
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, u: torch.Tensor, v: torch.Tensor):
+        z = torch.cat((u, v), dim=0)
+        N = z.size(0)
+        sim_zz = sim(z, z, self.temperature)
+
+        diag_mask = torch.eye(N, device=u.device, dtype=torch.bool)
+        pos_mask = diag_mask.roll(shifts=N // 2, dims=0) | diag_mask.roll(
+            shifts=-N // 2, dims=0
+        )
+        pos_loss = sim_zz.masked_select(pos_mask)
+
+        neg_mask = ~pos_mask & ~diag_mask
+        neg_sim_zz = sim_zz.masked_select(neg_mask).view(N, -1)
+        neg_loss = torch.logsumexp(neg_sim_zz, dim=-1)
+
+        return (-pos_loss + neg_loss).mean()
 
 
 if __name__ == "__main__":
@@ -96,3 +119,7 @@ if __name__ == "__main__":
     dhel = DHEL(temperature)
     dhel_loss = dhel(u, v)
     print("DHEL Loss:", dhel_loss.item())
+
+    nt_xent = NT_xent(temperature)
+    nt_xent_loss = nt_xent(u, v)
+    print("NT-Xent Loss:", nt_xent_loss.item())
