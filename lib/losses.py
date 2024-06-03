ac17ce541af793eba26c46756c71509347571a69
viig99 <accio.arjun@gmail.com>
2024-06-03 10:43:54 -0400

    Adding DHEL Loss.

--- lib/losses.py
+++ lib/losses.py
@@ -53,6 +53,28 @@ class DCL(torch.nn.Module):
         return (pos_loss + neg_loss).mean()
 
 
+class DHEL(torch.nn.Module):
+    def __init__(self, temperature: float = 0.07):
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, u: torch.Tensor, v: torch.Tensor):
+        # u: shape (B, D) where B is the batch size and D is the feature dimension
+        # v: shape (B, D) where B is the batch size and D is the feature dimension
+
+        B, _ = u.shape
+        sim_uv = sim(u, v, self.temperature)
+        sim_uu = sim(u, u, self.temperature)
+
+        pos_mask = torch.eye(B, device=u.device, dtype=torch.bool)
+        pos_loss = -sim_uv.masked_select(pos_mask)
+
+        neg_sim_uu = -sim_uu[~pos_mask].view(B, -1)
+        neg_loss = torch.logsumexp(neg_sim_uu, dim=-1)
+
+        return (pos_loss + neg_loss).mean()
+
+
 if __name__ == "__main__":
 
     torch.random.manual_seed(42)
@@ -70,3 +92,7 @@ if __name__ == "__main__":
     infonce = InfoNCELoss(temperature)
     infonce_loss = infonce(u, v)
     print("InfoNCE Loss:", infonce_loss.item())
+
+    dhel = DHEL(temperature)
+    dhel_loss = dhel(u, v)
+    print("DHEL Loss:", dhel_loss.item())
